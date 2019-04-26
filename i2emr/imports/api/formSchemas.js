2e5c75916d0365f6bc711ecba0004b9fe8f3e8bc
Andrelim435 <andrelim435@hotmail.com>
2019-04-26 19:03:35 +0800

    Add actual form fields
    And clean up Queue component

--- i2emr/imports/api/formSchemas.js
+++ i2emr/imports/api/formSchemas.js
@@ -35,42 +35,127 @@ export const formSchemas = {
         }
       }
     },
+    gender: {
+      type: String,
+      allowedValues: ['male', 'female'],
+    },
+    age: {
+      type: SimpleSchema.Integer,
+      min: 0,
+    },
+    contactNumber: {
+      type: SimpleSchema.Integer,
+    },
+    spokenLanguages: {
+      type: Array,
+    },
+    'spokenLanguages.$': {
+      type: String,
+      allowedValues: ['Sambalpuri', 'Odia', 'English', 'Others'],
+    },
+    writtenLanguages: {
+      type: Array,
+    },
+    'writtenLanguages.$': {
+      type: String,
+      allowedValues: ['Sambalpuri', 'Odia', 'English', 'Others'],
+    },
+    address: {
+      type: String,
+    },
+    anyDrugAllergies: {
+      type: Boolean,
+    },
+    drugAllergies: {
+      type: String,
+      optional: true,
+    },
   }),
 
   "Height & weight":
   new SimpleSchema({
     height: {
       type: Number,
+      min: 0.7,
+      max: 2.8,
+      label: "Height (m)",
     },
     weight: {
       type: Number,
+      min: 5,
+      max: 500,
+      label: "Weight (kg)",
     },
     waist: {
       type: Number,
+      label: "Waist circumference (cm)",
+    },
+    hip: {
+      type: Number,
+      label: "Hip circumference (cm)",
     },
   }),
 
   "CBG & Hb":
   new SimpleSchema({
     cbg: {
-      type: Number,
+      type: SimpleSchema.Integer,
+      min: 20,
+      max: 400,
+      label: "Capillary Blood Glucose (mg/dL)",
     },
     hb: {
       type: Number,
+      min: 4,
+      max: 40,
+      label: "Hemoglobin (g/dL)"
     },
   }),
 
   "Phlebotomy": 
   new SimpleSchema({
-    blood: {
-      type: Number,
+    phleboCompleted: {
+      type: Boolean,
     },
   }),
 
   "Blood pressure":
   new SimpleSchema({
-    bp: {
-      type: Number,
+    bp1Sys: {
+      type: SimpleSchema.Integer,
+      min: 50,
+      max: 300,
+      label: "1st Systolic blood pressure"
+    },
+    bp1Dia: {
+      type: SimpleSchema.Integer,
+      min: 20,
+      max: 200,
+      label: "1st Diastolic blood pressure"
+    },
+    bp2Sys: {
+      type: SimpleSchema.Integer,
+      min: 50,
+      max: 300,
+      label: "2nd Systolic blood pressure"
+    },
+    bp2Dia: {
+      type: SimpleSchema.Integer,
+      min: 20,
+      max: 200,
+      label: "2nd Diastolic blood pressure"
+    },
+    bp3Sys: {
+      type: SimpleSchema.Integer,
+      min: 50,
+      max: 300,
+      label: "3rd Systolic blood pressure"
+    },
+    bp3Dia: {
+      type: SimpleSchema.Integer,
+      min: 20,
+      max: 200,
+      label: "3rd Diastolic blood pressure"
     },
   }),
 }
