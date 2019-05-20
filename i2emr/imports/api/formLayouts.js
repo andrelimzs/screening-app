59f032c72834e7faac5f7edc4bef1f15a2311da2
Andrelim435 <andrelim435@hotmail.com>
2019-05-20 12:59:17 +0800

    Add custom AutoField to formLayouts

--- i2emr/imports/api/formLayouts.js
+++ i2emr/imports/api/formLayouts.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { formSchemas } from '/imports/api/formSchemas';
 
 import AutoForm from 'uniforms-material/AutoForm';
+import AutoField from 'uniforms-material/AutoField';
+import TextField from 'uniforms-material/TextField';
+import SubmitField from 'uniforms-material/SubmitField';
 
 // Define the layouts
 export const formLayouts = {
@@ -13,4 +16,17 @@ export const formLayouts = {
       onSubmit={this.handleSubmit}
       grid={12}
     />),
+
+  "Height & weight": (
+    <AutoForm ref={(ref) => this.formRef = ref} schema={formSchemas["Height & weight"]} onSubmit={this.handleSubmit}>
+      <TextField name="height" />
+      <TextField name="weight" />
+      <TextField name="waist" />
+      <TextField name="hip" />
+      <div>
+        <SubmitField />
+      </div>
+    </AutoForm>
+  )
+    
 };
