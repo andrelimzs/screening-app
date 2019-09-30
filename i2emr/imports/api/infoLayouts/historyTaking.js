b755a803ecf2e6cdf6efc8a2ae8cb20c7448f011
Jonathan Tan <jonathantjm@u.nus.edu>
2019-09-30 15:20:32 +0800

    update hx info

--- i2emr/imports/api/infoLayouts/historyTaking.js
+++ i2emr/imports/api/infoLayouts/historyTaking.js
@@ -6,6 +6,8 @@ export const historyTaking = (info) => {
         Booth number and History-taker's Name<br />
         <b>{typeof(info['Hx HCSR']) !== "undefined" &&
             info['Hx HCSR'].hxHcsrQ1}</b><br /><br />
+
+        <h2>Health Concerns</h2>
         Brief History <br/>
         <b>{typeof(info['Hx HCSR']) !== "undefined" &&
             info['Hx HCSR'].hxHcsrQ2.split('\n').map( text => {
@@ -40,5 +42,68 @@ export const historyTaking = (info) => {
             info['Hx HCSR'].hxHcsrQ9.split('\n').map( text => {
                 return <p>{text}<br/></p>
             })}</b><br /><br />
+
+        <h2>NSS</h2>
+        Relevant Past Medical History <br/>
+        <b>{typeof(info['Hx NSS']) !== "undefined" &&
+            typeof(info['Hx NSS'].hxNssQ12) !== "undefined" &&
+            info['Hx NSS'].hxNssQ12.split('\n').map( text => {
+                return <p>{text}<br/></p>
+            })}</b><br /><br />
+        Do you smoke? <br/>
+        <b>{typeof(info['Hx NSS']) !== "undefined" &&
+            info['Hx NSS'].hxNssQ14}</b><br /><br />
+        Do you consume alcoholic drinks? <br/>
+        <b>{typeof(info['Hx NSS']) !== "undefined" &&
+            info['Hx NSS'].hxNssQ15}</b><br /><br />
+
+        <h2>Social</h2>
+        Government Financial Assistance status <br/>
+        <b>{typeof(info['Hx Social']) !== "undefined" &&
+            info['Hx Social'].hxSocialQ1}<br />
+            {typeof(info['Hx Social']) !== "undefined" &&
+            typeof(info['Hx Social'].hxSocialQ2) !== "undefined" &&
+            info['Hx Social'].hxSocialQ2}</b><br /><br />
+        Average monthly earnings <br/>
+        <b>{typeof(info['Hx Social']) !== "undefined" &&
+            info['Hx Social'].hxSocialQ3}</b><br /><br />
+        Number of household members (including self) <br/>
+        <b>{typeof(info['Hx Social']) !== "undefined" &&
+            info['Hx Social'].hxSocialQ4}</b><br /><br />
+        Apply fo CHAS card? <br/>
+        <b>{typeof(info['Hx Social']) !== "undefined" &&
+            info['Hx Social'].hxSocialQ5}<br />
+            {typeof(info['Hx Social']) !== "undefined" &&
+            typeof(info['Hx Social'].hxSocialQ6) !== "undefined" &&
+            info['Hx Social'].hxSocialQ6.split('\n').map( text => {
+                return <p>{text}<br/></p>
+            })}</b><br /><br />
+        Do you need advice on financial assistance?
+        <b>{typeof(info['Hx Social']) !== "undefined" &&
+            info['Hx Social'].hxSocialQ7}<br />
+            {typeof(info['Hx Social']) !== "undefined" &&
+            typeof(info['Hx Social'].hxSocialQ8) !== "undefined" &&
+            info['Hx Social'].hxSocialQ8.split('\n').map( text => {
+                return <p>{text}<br/></p>
+            })}</b><br /><br />
+        Caring for loved one? <br/>
+        <b>{typeof(info['Hx Social']) !== "undefined" &&
+            info['Hx Social'].hxSocialQ9}</b><br /><br />
+        {
+            typeof(info['Hx Social']) !== "undefined" &&
+            info['Hx Social'].hxSocialQ9 === "Yes" &&
+            <Fragment>
+                Need training?
+                <b>{typeof(info['Hx Social']) !== "undefined" &&
+                info['Hx Social'].hxSocialQ10}</b><br /><br />
+                Need assistance?
+                <b>{typeof(info['Hx Social']) !== "undefined" &&
+                info['Hx Social'].hxSocialQ11}</b><br /><br />
+            </Fragment>
+        }
+        Need social support? <br/>
+        <b>{typeof(info['Hx Social']) !== "undefined" &&
+            info['Hx Social'].hxSocialQ12}</b><br />
+
     </Fragment>
 }
